4bd3c9506977058f08c28deb2c77192725db49ec
maximkrouk <id.maximkrouk@gmail.com>
2026-02-03 19:09:39 +0100

    feat(AVFoundation): Update _AVCaptureDeviceProtocol availability

--- Sources/AVFoundationMarkerProtocols/_AVCaptureDeviceProtocol.swift
+++ Sources/AVFoundationMarkerProtocols/_AVCaptureDeviceProtocol.swift
@@ -1,9 +1,11 @@
 #if canImport(AVFoundation) && !os(watchOS)
 import AVFoundation
 
-@available(macOS 10.7, iOS 4.0, macCatalyst 14.0, tvOS 17.0, visionOS 1.0, *)
+@available(iOS 4.0, macOS 10.7, macCatalyst 14.0, tvOS 17.0, visionOS 1.0, *)
+@available(watchOS, unavailable)
 public protocol _AVCaptureDeviceProtocol: AVCaptureDevice {}
 
-@available(macOS 10.7, iOS 4.0, macCatalyst 14.0, tvOS 17.0, visionOS 1.0, *)
+@available(iOS 4.0, macOS 10.7, macCatalyst 14.0, tvOS 17.0, visionOS 1.0, *)
+@available(watchOS, unavailable)
 extension AVCaptureDevice: _AVCaptureDeviceProtocol {}
 #endif

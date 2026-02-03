cd592e8dad9ba18bc2f253461e76e822168991f6
maximkrouk <id.maximkrouk@gmail.com>
2026-02-03 19:09:49 +0100

    feat(QuartzCore): Update _CALayer availability

--- Sources/QuartzCoreMarkerProtocols/_CALayerProtocol.swift
+++ Sources/QuartzCoreMarkerProtocols/_CALayerProtocol.swift
@@ -1,6 +1,11 @@
 #if canImport(QuartzCore)
 import QuartzCore
 
+@available(iOS 2.0, macOS 10.5, macCatalyst 13.1, tvOS 9.0, visionOS 1.0, *)
+@available(watchOS, unavailable)
 public protocol _CALayerProtocol: CALayer {}
+
+@available(iOS 2.0, macOS 10.5, macCatalyst 13.1, tvOS 9.0, visionOS 1.0, *)
+@available(watchOS, unavailable)
 extension CALayer: _CALayerProtocol {}
 #endif

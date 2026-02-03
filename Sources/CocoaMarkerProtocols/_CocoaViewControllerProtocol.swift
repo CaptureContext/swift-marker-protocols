66fde84d97d8d8e77154121b5c39bd752a655d4e
maximkrouk <id.maximkrouk@gmail.com>
2026-02-03 19:14:48 +0100

    feat(Cocoa): Update ViewController protocols availability

--- Sources/CocoaMarkerProtocols/_CocoaViewControllerProtocol.swift
+++ Sources/CocoaMarkerProtocols/_CocoaViewControllerProtocol.swift
@@ -1,7 +1,12 @@
 #if canImport(UIKit)
 import UIKit
 
+@available(iOS 2.0, macCatalyst 13.1, visionOS 1.0, *)
+@available(watchOS, unavailable)
 public protocol _UIViewControllerProtocol: UIViewController {}
+
+@available(iOS 2.0, macCatalyst 13.1, visionOS 1.0, *)
+@available(watchOS, unavailable)
 extension UIViewController: _UIViewControllerProtocol {}
 
 #elseif os(macOS) && canImport(AppKit)
